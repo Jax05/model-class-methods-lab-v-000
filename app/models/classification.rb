ebc4fc2a4a325a09f7d1c8e3f7750e6132dd86bd
Carley Tripp <carley.tripp@outlook.com>
2019-03-15 02:06:47 +0000

    Automatically backed up by Learn

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -7,6 +7,6 @@ class Classification < ActiveRecord::Base
   end
 
   def self.longest
-    includes(boats: [:length]).order("length desc")
+    includes(boats: [:lengths]).order("length desc")
   end
 end
